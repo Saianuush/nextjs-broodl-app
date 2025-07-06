0c225b28e042401864caf31c6ef6117d779337c8
Saianuush <saianuushat2004@gmail.com>
2025-07-06 19:19:59 +0530

    made header and footer of dashboard page

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fugaz_One,Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const fugaz = Fugaz_One({subsets: ["latin"], weight: ['400']});
+const opensans = Open_Sans({subsets: ["latin"]});
 
 export const metadata = {
   title: "Broodl",
@@ -17,18 +10,26 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4 ">
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
+    </header>
+  )
+
+  const footer = (
+    <footer className="p-4 sm:p-8 grid place-items-center">
+      <p className={'text-indigo-500 ' + fugaz.className}>Created with ❤️</p>
+    </footer>
+  )
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800' + opensans.className} 
         >
-        <header>
-          Header
-        </header>
+        {header}
         {children}
-        <footer>
-          Footer
-        </footer>
+        {footer}
       </body>
     </html>
   );
